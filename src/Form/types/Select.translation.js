56bef82a6bf25234c5b9249441b08e3dda204395
Oscar Friberg <oscfriberg@gmail.com>
2020-07-02 11:49:36 +0200

    temp2

---
+++ src/Form/types/Select.translation.js
@@ -0,0 +1,11 @@
+import addTranslation from '../../../lib/i18n/addTranslation';
+
+export default addTranslation({
+  id: 'formSelect',
+  sv: {
+    reset: 'Återställ',
+  },
+  en: {
+    reset: 'Reset',
+  },
+});
