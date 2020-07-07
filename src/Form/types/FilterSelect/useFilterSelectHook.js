0fceca40b77255c2b90aac1e9bc257a0acf3024d
Mikael Hallgren <hallgren.mikael@gmail.com>
2020-07-07 13:52:13 +0200

    Create custom multiselect hook

---
+++ src/Form/types/FilterSelect/useFilterSelectHook.js
@@ -0,0 +1,124 @@
+import {
+  useEffect, useState, useMemo,
+} from 'react';
+
+const getDetfaultValue = (values) => {
+  if (!values) { return []; }
+  if (Array.isArray(values)) { return values; }
+  return [values];
+};
+
+const getDetfaultSingleValue = (values, options) => {
+  const parseValue = getDetfaultValue(values);
+  if (parseValue.length > 0) {
+    return parseValue;
+  }
+  if ((!values || (Array.isArray(values) && values.length === 0)) && options.length > 0) {
+    return [options[0]];
+  }
+
+  return [];
+};
+
+const useTableHook = (values, options, multiSelect, outputParser) => {
+  const [isOpen, setOpen] = useState(false);
+  const [search, setSearch] = useState('');
+  const [selectedOptions, setSelected] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [isReady, setReady] = useState(false);
+
+  useEffect(() => {
+    if (isReady === false && options && options.length > 0) {
+      if (!multiSelect) {
+        setSelected(getDetfaultSingleValue(values, options));
+      } else {
+        setSelected(getDetfaultValue(values));
+      }
+      setReady(true);
+    }
+  }, [values, options, multiSelect, selectedOptions, isReady]);
+
+  useEffect(() => {
+    setInputValue(outputParser(selectedOptions));
+  }, [outputParser, selectedOptions]);
+
+  const listOptions = useMemo(() => {
+    if (options && Array.isArray(options)) {
+      const selected = getDetfaultValue(values);
+      const mergedOptions = Array.from(new Set([
+        ...selected,
+        ...options,
+      ])).reduce((acc, item) => [{
+        value: item,
+        selected: selectedOptions.some((val) => val === item),
+      }, ...acc], []);
+
+      const filterd = mergedOptions
+        .filter((item) => {
+          if (item.static === true) {
+            return true;
+          }
+
+          const label = item.label || item.value;
+
+          if (label) {
+            return label
+              .toString()
+              .toLowerCase()
+              .match(search.toString().toLowerCase());
+          }
+          return false;
+        })
+        .sort((a, b) => {
+          const textA = a.value.toUpperCase();
+          const textB = b.value.toUpperCase();
+          if (textA < textB) { return -1; }
+          if (textA > textB) { return 1; }
+          return 0;
+        })
+        .sort((a, b) => {
+          if (a.selected && !b.selected) { return -1; }
+          if (!a.selected && b.selected) { return 1; }
+          return 0;
+        });
+
+      return filterd;
+    }
+    return [];
+  }, [options, search, selectedOptions, values]);
+
+
+  return {
+    isOpen,
+    setOpen,
+    search,
+    setSearch,
+    hasOptions: () => listOptions.length > 0,
+    getOptions: () => listOptions,
+    hasSelected: () => selectedOptions.length > 0,
+    getSelected: () => selectedOptions,
+    getInputValue: () => inputValue,
+    showTags: () => Boolean(multiSelect) && selectedOptions.length > 0,
+    getTags: () => selectedOptions.map((val) => ({ value: val })),
+    selectItem: (item) => {
+      if (multiSelect) {
+        if (item.selected) {
+          const result = selectedOptions.filter((value) => value !== item.value);
+          setSelected(result);
+
+          return result;
+        }
+
+        const result = Array.from(new Set([item.value, ...selectedOptions]));
+        setSelected(result);
+
+        return result;
+      }
+
+      setSelected([item.value]);
+      return item.value;
+    },
+  };
+};
+
+export default useTableHook;
