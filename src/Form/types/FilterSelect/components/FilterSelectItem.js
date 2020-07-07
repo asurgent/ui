c6146cb5376ff0851d375dbcf7e630d38cb05585
Mikael Hallgren <hallgren.mikael@gmail.com>
2020-07-07 13:51:57 +0200

    Multiselect items

---
+++ src/Form/types/FilterSelect/components/FilterSelectItem.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as Icons from '@material-ui/icons';
+import * as C from './FilterSelectItem.styled';
+
+const propTypes = {
+  filterItem: PropTypes.instanceOf(Object).isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+const defaultProps = {};
+
+
+const FilterItem = ({
+  filterItem,
+  onChange,
+}) => (
+  <C.FilterItem onClick={() => onChange(filterItem)}>
+    <C.Active>
+      {filterItem.selected && <Icons.Check />}
+    </C.Active>
+    <C.FilterLabel>
+      { filterItem.value }
+    </C.FilterLabel>
+  </C.FilterItem>
+);
+
+FilterItem.propTypes = propTypes;
+FilterItem.defaultProps = defaultProps;
+FilterItem.displayName = '@asurgent.ui.Form.FilterSelect.FilterSelectItem';
+
+export default FilterItem;
---
+++ src/Form/types/FilterSelect/components/FilterSelectItem.styled.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+
+export const Base = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1.6rem;
+`;
+
+export const Active = styled(Base)`
+    width: 3.2rem;
+`;
+
+
+export const FilterItem = styled.div`
+    display: flex;
+    flex-direction: row;
+    border-bottom: 1px solid ${({ theme }) => theme.gray200};    
+    opacity: ${({ matched }) => (matched ? 1 : 0.5)};
+    cursor: pointer;
+
+    &:hover {
+        background: ${({ theme }) => theme.gray50};
+    }
+    pointer-events: ${({ disabled }) => (disabled ? 'none' : 'all')};
+`;
+
+export const FilterLabel = styled.div`
+    flex: 1;
+    display: block;
+    text-align: left;
+    font-size: 1.1rem;
+    font-weight: 700;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 1.6rem;
+    max-width: 29.5rem;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+`;
+export const Labels = styled.div`
+    width: 100%;
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+export const Label = styled.div`
+    flex: 1;
+    display: block;
+    text-align: left;
+    font-size: 1.1rem;
+    font-weight: 700;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 1.6rem;
+    max-width: 29.5rem;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+`;
+
+export const SecondaryLabel = styled(Label)`
+    color: ${({ theme }) => theme.gray400};
+    font-size: 1rem;
+`;
