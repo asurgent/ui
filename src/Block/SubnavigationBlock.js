b1be526e30e4e6461220f603ac0b9df67c416857
Mikael Hallgren <hallgren.mikael@gmail.com>
2020-07-16 13:41:15 +0200

    Create subnavigation block component

---
+++ src/Block/SubnavigationBlock.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as C from './SubnavigationBlock.styled';
+
+const propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]),
+  navigationList: PropTypes.instanceOf(PropTypes.Array).isRequired,
+};
+
+const defaultProps = {
+  children: null,
+};
+
+const Emptystate = ({ navigationList, children }) => (
+  <C.Wrapper>
+    <C.Navigation>
+      {
+        navigationList.map(({
+          path,
+          icon,
+          label,
+          isActive,
+        }) => (
+          <C.NavigationItem to={path} key={path} isActive={isActive}>
+            <C.Icon>{icon}</C.Icon>
+            <C.Label>{label}</C.Label>
+          </C.NavigationItem>
+        ))
+      }
+    </C.Navigation>
+    <C.Content>
+      {children}
+    </C.Content>
+  </C.Wrapper>
+);
+
+Emptystate.propTypes = propTypes;
+Emptystate.defaultProps = defaultProps;
+Emptystate.displayName = '@asurgent.ui.Block.Emptystate';
+
+export default Emptystate;
---
+++ src/Block/SubnavigationBlock.styled.js
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    padding: 3.2rem;
+`;
+export const Navigation = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 25rem;
+    margin-right: 3.2rem;
+`;
+export const NavigationItem = styled(NavLink)`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    height: 3.2rem;
+    color: ${({ theme }) => theme.black};
+    font-size: 1.6rem;
+    line-height: 2rem;
+    border-left: 2px solid transparent;
+    margin-bottom: .8rem;
+
+    &.active {
+        background-color: #F9F9F9;
+        border-color: #133A5D;
+        font-weight: bold;
+    }
+`;
+
+export const Icon = styled.div`
+    margin-left: 1.6rem;
+    margin-right: 1.6rem;
+`;
+export const Label = styled.div``;
+export const Content = styled.div`
+    h1 {
+        font-family: "Poppins";
+        font-style: normal;
+        font-weight: normal;
+        font-size: 23.04px;
+        line-height: 28px;
+    }
+
+    h2 {
+        padding: 0;
+        margin-top: 0;
+        font-family: "Poppins";
+        font-style: normal;
+        font-weight: 500;
+        font-size: 19.2px;
+        line-height: 24px;
+    }
+`;
