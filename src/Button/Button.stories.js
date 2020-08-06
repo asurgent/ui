57bd66d22f8136b61b496c57c2469398c5f706f8
Oscar Friberg <oscfriberg@gmail.com>
2020-08-06 14:33:19 +0200

    Click actions for all buttons in story

--- src/Button/Button.stories.js
+++ src/Button/Button.stories.js
@@ -12,12 +12,12 @@ export default { title: 'UI Components|Buttons', decorators: [withKnobs] };
 
 export const saveToFileButton = () => (
   <>
-    <Button.Icon saveToFilename="some_file" tooltip="Saves from promise" saveToJson={() => new Promise((r) => r(['a', 'b']))} icon={<Icon.ConfirmationNumber fontSize="large" />} />
-    <Button.Icon saveToFilename="another_file" tooltip="Saves from object" saveToJson={() => ['a', 'b']} icon={<Icon.ConfirmationNumber fontSize="large" />} />
+    <Button.Icon saveToFilename="some_file" tooltip="Saves from promise" saveToJson={() => new Promise((r) => r(['a', 'b']))} icon={<Icon.ConfirmationNumber fontSize="large" onClick={() => action('Click!')} />} />
+    <Button.Icon saveToFilename="another_file" tooltip="Saves from object" saveToJson={() => ['a', 'b']} icon={<Icon.ConfirmationNumber fontSize="large" onClick={() => action('Click!')} />} />
   </>
 );
 export const iconButton = () => (
-  <Button.Icon tooltip="Hello" saveLinkState link="/test" icon={<Icon.ConfirmationNumber fontSize="large" />} />
+  <Button.Icon tooltip="Hello" saveLinkState link="/test" icon={<Icon.ConfirmationNumber fontSize="large" onClick={() => action('Click!')} />} />
 );
 
 export const submitButton = () => (
@@ -31,47 +31,47 @@ export const submitButton = () => (
 );
 
 export const primaryButton = () => (
-  <Button.Primary loading={boolean('loading', true)}>{content}</Button.Primary>
+  <Button.Primary loading={boolean('loading', true)} onClick={() => action('Click!')}>{content}</Button.Primary>
 );
 
 export const secondaryButton = () => (
-  <Button.Secondary>{content}</Button.Secondary>
+  <Button.Secondary onClick={() => action('Click!')}>{content}</Button.Secondary>
 );
 
 export const hollowButton = () => (
-  <Button.Hollow>{content}</Button.Hollow>
+  <Button.Hollow onClick={() => action('Click!')}>{content}</Button.Hollow>
 );
 
 export const hrefButton = () => (
-  <Button.Primary disabled link="https://google.com">{content}</Button.Primary>
+  <Button.Primary disabled link="https://google.com" onClick={() => action('Click!')}>{content}</Button.Primary>
 );
 
 export const plainButton = () => (
-  <Button.Plain>{content}</Button.Plain>
+  <Button.Plain onClick={() => action('Click!')}>{content}</Button.Plain>
 );
 
 export const linkButton = () => (
-  <Button.Link iconLeft={<Icon.ConfirmationNumber fontSize="medium" />}>{content}</Button.Link>
+  <Button.Link iconLeft={<Icon.ConfirmationNumber fontSize="medium" onClick={() => action('Click!')} />}>{content}</Button.Link>
 );
 
 export const transparentButton = () => (
-  <Button.Transparent>{content}</Button.Transparent>
+  <Button.Transparent onClick={() => action('Click!')}>{content}</Button.Transparent>
 );
 
 export const rejectButton = () => (
-  <Button.Reject>{content}</Button.Reject>
+  <Button.Reject onClick={() => action('Click!')}>{content}</Button.Reject>
 );
 
 export const acceptButton = () => (
-  <Button.Create>{content}</Button.Create>
+  <Button.Create onClick={() => action('Click!')}>{content}</Button.Create>
 );
 
 export const pillButton = () => (
-  <Button.Pill>{content}</Button.Pill>
+  <Button.Pill onClick={() => action('Click!')}>{content}</Button.Pill>
 );
 
 export const tooltipButton = () => (
-  <Button.Primary tooltip="hello" tooltipOrientation={text('orientation (middle,left,right)', 'middle')}>
+  <Button.Primary tooltip="hello" tooltipOrientation={text('orientation (middle,left,right)', 'middle')} onClick={() => action('Click!')}>
     {content}
   </Button.Primary>
 );
