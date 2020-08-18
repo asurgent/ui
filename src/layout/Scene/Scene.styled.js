ecc8d87a904cb5ffc528e1c53d6c685ace6667af
Mikael Hallgren <hallgren.mikael@gmail.com>
2020-08-18 16:00:31 +0200

    Simplify scene component and make it work in safari

--- src/layout/Scene/Scene.styled.js
+++ src/layout/Scene/Scene.styled.js
@@ -4,16 +4,8 @@ export const Scene = styled.div`
     background: ${({ theme }) => theme.white};
     position: relative;
     overflow-y: auto;
-    overflow-x: auto;
-    height: 100%;
-    max-height: 100%;
-    min-height: 100%;
-    width: 100%;
-    max-width: 100%;
-    min-width: 100%;
     overflow-x: hidden;
-    display: flex;
-    flex-direction: column;
+    display: block;
     padding-bottom: 3.2rem;
 
     @media screen and (min-width: ${(prop) => `${prop.theme.breakPointDesktop * 10}px`}) {
