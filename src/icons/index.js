b1722b4401943bbb4e57754f45970788fef3f9bd
Oscar Friberg <oscfriberg@gmail.com>
2020-10-02 14:30:24 +0200

    add vm icon to svg-component

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,9 +1,11 @@
 import IconAsurgent from './IconAsurget';
 import IconNoTickets from './IconNoTickets';
 import IconConnection from './IconConnection';
+import IconVM from './IconVM';
 
 export {
   IconAsurgent,
   IconNoTickets,
   IconConnection,
+  IconVM,
 };
