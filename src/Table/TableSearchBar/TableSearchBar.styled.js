08a503a514cfff2acb4fb398db8c1f9b22e51f3b
Oscar Friberg <oscfriberg@gmail.com>
2020-11-11 13:09:32 +0100

    back to inline svg...

--- src/Table/TableSearchBar/TableSearchBar.styled.js
+++ src/Table/TableSearchBar/TableSearchBar.styled.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import Icon from './Icon.svg';
 
 export const SearchBarContainer = styled.div`
     position: relative;
@@ -13,7 +12,7 @@ export const SearchBarContainer = styled.div`
         }
      
         &:before {
-          content: url(${Icon});
+          content: url("data:image/svg+xml; utf8, <svg xmlns='http://www.w3.org/2000/svg' aria-hidden='true' focusable='false' width='100%' height='100%' preserveAspectRatio='xMidYMid meet' viewBox='0 0 24 24'><path d='M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5A6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5S14 7.01 14 9.5S11.99 14 9.5 14z' fill='rgb(230, 230, 230)'/></svg>");
           position: absolute;
           left: 1.4rem;
           top: 1.1rem;
