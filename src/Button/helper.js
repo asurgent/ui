4f6ce4533b09d98aee5d994ccbc12e7c8ee9c2fd
Oscar Friberg <oscfriberg@gmail.com>
2020-11-12 16:06:05 +0100

    handle no data & different types (csv/json & txt-fallback)

--- src/Button/helper.js
+++ src/Button/helper.js
@@ -1,37 +1,67 @@
 export const isExternalLink = (link) => (link || '').toString().match(/^(http(s)?:\/\/)/);
 export const isInteralLink = (link) => (link || '').toString().match(/^(\/)/);
 export const isValidMail = (link) => (link || '').toString().match(/^(.+@.+\.[a-zAZ]+)$/);
+const JSONToCSV = ({ data, delimiter = ',' }) => {
+  if (!Array.isArray(data)
+  || data?.length === 0
+  || Object.keys(data[0])?.length === 0) {
+    return null;
+  }
 
-const JSONToCSV = ({ data = [], delimiter = ',' }) => {
-  const cols = data.length > 0 ? Object.keys(data[0]) : [];
-
-  return [
+  const cols = Object.keys(data[0]);
+  const res = [
     cols.join(delimiter),
     ...data.map((obj) => cols.reduce(
       (acc, key) => `${acc}${acc.length ? delimiter : ''}"${obj[key] || ''}"`,
       '',
     )),
   ].join('\n');
+
+  return res;
 };
 
-export const fileSaver = async ({
-  data = [],
-  type = 'csv',
-  fileName = 'export',
-  overrideConfig,
-}) => {
-  const config = {
-    type: type === 'csv' ? 'data:text/csv' : 'application/json',
-    fileExtension: type,
-    ...overrideConfig,
-  };
-  const fixedData = type === 'csv' ? JSONToCSV({ data }) : JSON.stringify(data);
+const stringifyData = (d) => {
+  try {
+    return JSON.stringify(d);
+  } catch (e) {
+    return '';
+  }
+};
 
-  const blob = new Blob([fixedData], { type: config.type });
+const getHandledData = (data, fileName) => {
+  if (!data) {
+    return { data: 'no data', type: 'text/plain', extension: 'txt' };
+  }
+
+  const lastPartFilename = fileName?.split('.').pop();
+  const fileExtension = ['csv', 'json'].includes(lastPartFilename) ? lastPartFilename : 'txt';
+
+  switch (fileExtension) {
+    case 'csv':
+      return {
+        data: JSONToCSV({ data }) || stringifyData(data),
+        type: 'text/csv',
+        extension: 'csv',
+      };
+    case 'json':
+      return {
+        data: stringifyData(data),
+        type: 'application/json',
+        extension: 'json',
+      };
+    default:
+      return { data: stringifyData(data), type: 'text/plain', extension: 'txt' };
+  }
+};
+
+export const fileSaver = async ({ data, fileName = 'export.csv' }) => {
+  const handledData = getHandledData(data, fileName);
+
+  const blob = new Blob([handledData.data], { type: handledData.type });
   const href = await URL.createObjectURL(blob);
   const link = document.createElement('a');
   link.href = href;
-  link.download = `${fileName}.${config.fileExtension}`;
+  link.download = `${fileName.replace(handledData.extension, '')}.${handledData.extension}`;
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
