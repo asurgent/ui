f52dfebe4dd50da3f163fb669d73eb403736713e
Oscar Friberg <oscfriberg@gmail.com>
2020-11-16 13:43:21 +0100

    Gray option on T.Paragraph, story & icon

--- src/icons/index.js
+++ src/icons/index.js
@@ -2,10 +2,12 @@ import IconAsurgent from './IconAsurget';
 import IconNoTickets from './IconNoTickets';
 import IconConnection from './IconConnection';
 import IconVM from './IconVM';
+import IconApplicationMonitoring from './IconApplicationMonitoring';
 
 export {
   IconAsurgent,
   IconNoTickets,
   IconConnection,
   IconVM,
+  IconApplicationMonitoring,
 };
