90a4aecd9603dbc3aa30cd5a4e4fc108f5cdf9cc
Oscar Friberg <oscfriberg@gmail.com>
2020-11-30 12:05:03 +0100

    font-family on svg text

--- src/Graph/Heatmap/components/Legend/Legend.styled.js
+++ src/Graph/Heatmap/components/Legend/Legend.styled.js
@@ -2,8 +2,10 @@ import styled from 'styled-components';
 
 export const Legend = styled.g`
     font-size: 12px;
+    font-family: 'Lato';
 `;
 
 export const Text = styled.text`
     font-size: 12px;
+    font-family: 'Lato';
 `;
