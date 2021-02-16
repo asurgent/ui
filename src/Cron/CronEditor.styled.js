b92497850db11e88110bae575e12587aa5e68330
Oscar Friberg <oscfriberg@gmail.com>
2021-02-16 16:12:18 +0100

    croneditor width fix

--- src/Cron/CronEditor.styled.js
+++ src/Cron/CronEditor.styled.js
@@ -47,7 +47,12 @@ export const MonthSlector = styled(WeekSelector)`
     grid-template-rows: repeat(5, 1fr);
 `;
 
-export const Editor = styled.div``;
+export const Editor = styled.div`
+    width: 100%;
+    @media screen and (min-width: 1024px) {
+        max-width: 700px;
+    }
+`;
 
 export const Text = styled.div`
     margin-bottom: .8rem;
