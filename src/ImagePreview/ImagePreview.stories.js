f22a630e51fdb73a642e245367d497a7d2a5e012
Oscar Friberg <oscfriberg@gmail.com>
2021-03-18 12:53:40 +0100

    comment in story

--- src/ImagePreview/ImagePreview.stories.js
+++ src/ImagePreview/ImagePreview.stories.js
@@ -9,9 +9,13 @@ const Story = {
       control: {
         type: 'radio',
         options: [
+          // wide
           'http://images6.fanpop.com/image/photos/36000000/Frodo-Sam-image-frodo-and-sam-36091190-1920-796.jpg',
+          // tall
           'https://www.funkopopmania.it/wp-content/uploads/2019/04/film-gandalf-il-signore-degli-anelli.jpg',
+          // square
           'https://1.bp.blogspot.com/-p-NCtut9uAE/UX1tk4qtfAI/AAAAAAAAA5U/jbG41mAFxPE/s1600/Sam_and_Rosie_at_their_wedding+(1249x532).jpg',
+          // invalid link
           'broken',
         ],
       },
