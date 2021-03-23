bfce93a7dc9d679f580099ba571dc4f87c1921ee
Oscar Friberg <oscfriberg@gmail.com>
2021-03-23 14:40:23 +0100

    eslint mad about destructuring-assignment in stories fix

--- src/Block/Block.stories.js
+++ src/Block/Block.stories.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import MdiIcon from '@mdi/react';
 import { mdiMenuDown } from '@mdi/js';
--- src/VirtualRender/VirtualRender.stories.js
+++ src/VirtualRender/VirtualRender.stories.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/destructuring-assignment */
 import React from 'react';
 import styled from 'styled-components';
 import * as VirtualRender from './index';
